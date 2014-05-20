836db7ca91f1e5fbb482b7a2cf0a0a24f83188ae
Gambit <gam6itko@gmail.com>
2014-05-20 19:35:55 +0400

    comments more readable

--- src/OneDrive/Enum/Scopes.php
+++ src/OneDrive/Enum/Scopes.php
@@ -11,7 +11,8 @@ class Scopes {
     const BASIC = 'wl.basic';
 
     /**
-     * The ability of an app to read and update a user's info at any time. Without this scope, an app can access the user's info only while the user is signed in to their Microsoft account and is using your app.
+     * The ability of an app to read and update a user's info at any time.
+     * Without this scope, an app can access the user's info only while the user is signed in to their Microsoft account and is using your app.
      */
     const OFFLINE_ACCESS = 'wl.offline_access';
 
@@ -39,7 +40,8 @@ class Scopes {
     const CALENDARS_UPDATE = 'wl.calendars_update';
 
     /**
-     * Read access to the birth day and birth month of a user's contacts. Note that this also gives read access to the user's birth day, birth month, and birth year.
+     * Read access to the birth day and birth month of a user's contacts.
+     * Note that this also gives read access to the user's birth day, birth month, and birth year.
      */
     const CONTACTS_BIRTHDAY ='wl.contacts_birthday';
 
@@ -49,17 +51,20 @@ class Scopes {
     const CONTACTS_CREATE = 'wl.contacts_create';
 
     /**
-     * Read access to a user's calendars and events. Also enables read access to any calendars and events that other users have shared with the user.
+     * Read access to a user's calendars and events.
+     * Also enables read access to any calendars and events that other users have shared with the user.
      */
     const CONTACTS_CALENDARS = 'wl.contacts_calendars';
 
     /**
-     * Read access to a user's albums, photos, videos, and audio, and their associated comments and tags. Also enables read access to any albums, photos, videos, and audio that other users have shared with the user.
+     * Read access to a user's albums, photos, videos, and audio, and their associated comments and tags.
+     * Also enables read access to any albums, photos, videos, and audio that other users have shared with the user.
      */
     const CONTACTS_PHOTOS = 'wl.contacts_photos';
 
     /**
-     * Read access to Microsoft OneDrive files that other users have shared with the user. Note that this also gives read access to the user's files stored in OneDrive.
+     * Read access to Microsoft OneDrive files that other users have shared with the user.
+     * Note that this also gives read access to the user's files stored in OneDrive.
      */
     const CONTACTS_SKYDRIVE = 'wl.contacts_skydrive';
 
